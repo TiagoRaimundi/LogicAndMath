68e824a2b8c2eeee25dcce583a078f120b37f9f2
Tiago Raimundi <raimunditiagoaugusto@gmail.com>
2023-09-26 20:35:02 -0300

    logic1

---
+++ logic1.js
@@ -0,0 +1,6 @@
+//Variáveis e tipos de dados: Escreva um programa que declare duas variáveis, “nome” e “idade”, e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+
+let nome = "João"
+let idade = 18
+
+console.log(`Olá meu nome é ${nome} e eu tenho ${idade} anos `)
