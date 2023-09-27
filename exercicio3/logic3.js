cb206b35359ed8785c16d18060600caeac2f8a07
Tiago Raimundi <raimunditiagoaugusto@gmail.com>
2023-09-27 07:01:41 -0300

    logic3

---
+++ exercicio3/logic3.js
@@ -0,0 +1,10 @@
+//Condições: Escreva um programa que pergunte ao usuário sua idade e imprima se ele é maior ou menor de idade.
+
+var idade = parseInt(prompt("Digite a sua idade: "))
+
+if(idade >= 18){
+    console.log('Você é maior de idade!')
+
+}else{
+    console.log('Você é menor de idade!')
+}
