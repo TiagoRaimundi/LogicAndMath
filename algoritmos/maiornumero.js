50bf48ddb840b603f13c22e1401907d6d4c8e611
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2024-01-05 13:46:19 -0300

    exercises

--- algoritmos/maiornumero.js
+++ algoritmos/maiornumero.js
@@ -1,15 +1,14 @@
 function EncontrarMaiorNumero(array){
     let maior = array[0]
-    for (let i = 1; i < array.length; i++){
-        if (array[i] > maior){
-             maior = array[i]
+    for(let i = 1; i < array.length; i ++){
+        if(array[i] > maior){
+            maior = array[i]
         }
 
-    }
+    } 
     return maior
-
 }
 
-const numbers = [ 1, 2, 5, 4]
-const result =  EncontrarMaiorNumero(numbers)
+const num = [1, 5, 7, 4 , 4552, 10, 54, 9, 71,]
+const result= EncontrarMaiorNumero(num)
 console.log(result)
