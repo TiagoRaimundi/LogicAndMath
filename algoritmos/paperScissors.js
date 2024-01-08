ca107b0d52aa4d404dabbd3890c6ed257bc56195
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2024-01-07 21:30:58 -0300

    practice

--- algoritmos/paperScissors.js
+++ algoritmos/paperScissors.js
@@ -1,6 +1,6 @@
 const readline = require('readline').createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
 })
 
 const escolhas = ['pedra', 'papel', 'tesoura']
--- algoritmos/somaArray.js
+++ algoritmos/somaArray.js
@@ -2,11 +2,14 @@ function somarNumeros(array){
     let soma = 0 
     for (i = 0; i < array.length; i++){
         soma += array[i]
+
+            
     }
 
     return soma
+
 }
 
-const number = [55, 64, 84]
-const result = somarNumeros(number)
+const numbers = [7, 8, 8]
+const result = somarNumeros(numbers)
 console.log(result)
