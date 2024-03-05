53c8541d6d6c6636a709567b929ad773ac6c78f1
Tiago Raimundi <88736718+Tiagoyeh@users.noreply.github.com>
2024-03-05 09:03:07 -0300

    binary search js

--- leetcode/ReplaceElements.js
+++ leetcode/ReplaceElements.js
@@ -1,16 +1,5 @@
-function replaceElements(arr) {
-    let maxRight = -1; // Initialize the maximum value for elements to the right as -1
-    
-    // Iterate through the array from right to left
-    for (let i = arr.length - 1; i >= 0; i--) {
-        const currentElement = arr[i];
-        arr[i] = maxRight; // Replace the current element with the maximum value to its right
-        maxRight = Math.max(maxRight, currentElement); // Update the maximum value encountered so far
-    }
-    
-    return arr;
-}
+let age = 28
+age = "John"
 
-// Example usage:
-const arr = [17, 18, 5, 4, 6, 1];
-console.log(replaceElements(arr)); // Output: [18, 6, 6, 6, 1, -1]
+
+console.log(age)
